0a5b6bcd038f9a88bfaf631fa034e83f8dff81e8
John Moon <johnmoonyy@gmail.com>
2014-01-01 18:56:45 -0500

    Initial install of gems and database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UsedCarLot::Application.config.secret_key_base = 'ddb3c44e13ea72a64d44445da2133fb031898251b221dab8f9aeb8ef0ebd7cce158aee9f75a3861524ed88246813e18fb65678648986b62bdb1a9562bc5a2d2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UsedCarLot::Application.config.session_store :cookie_store, key: '_used_car_lot_session'
