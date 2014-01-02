8ddaaafcdbfdeeb1d07e028b6d15c0fab4cff2a8
John Moon <johnmoonyy@gmail.com>
2014-01-01 20:01:04 -0500

    Add controllers to view all car listing and create new car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,27 @@
 class CarsController < ApplicationController
+  def index
+    @car = Car.all
+  end
+
   def new
     @car = Car.new
   end
 
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      flash[:notice] = "Successfully created car"
+      redirect_to cars_path
+    else
+      flash[:notice] = "Please input valid information"
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:color, :year, :mileage)
+  end
 end
--- spec/features/user_adds_car_spec.rb
+++ spec/features/user_adds_car_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 feature 'user' do
+  given(:car) { FactoryGirl.create(:car) }
+
   scenario 'inputs valid information' do
     visit new_car_path
-    fill_in :color, with: car.color
-    fill_in :year, with: car.year
-    fill_in :mileage, with: car.mileage
+    fill_in :car_color, with: car.color
+    fill_in :car_year, with: car.year
+    fill_in :car_mileage, with: car.mileage
     click_on "submit"
 
     expect(page).to have_content("Add Car")
@@ -13,9 +15,16 @@ feature 'user' do
 
   scenario 'inputs invalid information' do
     visit new_car_path
-
     click_on "submit"
 
-    expect(page).to have_content("Please input valid information")
+    error_message(:color)
+    error_message(:year)
+    error_message(:mileage)
+  end
+
+  def error_message(attributes)
+    within(".input.car_#{attributes.to_s}") do
+      expect(page).to have_content "can't be blank"
+    end
   end
 end
